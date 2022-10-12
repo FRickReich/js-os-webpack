726a0538ac4e2cb60190f9115d3510084b237117
FRickReich <frederikreich@googlemail.com>
2022-10-12 15:30:23 +0200

    3 files modified

--- webpack.config.js
+++ webpack.config.js
@@ -21,4 +21,11 @@ module.exports = {
             },
         ],
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
 };
